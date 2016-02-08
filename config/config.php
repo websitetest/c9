1618d05a0f2652a4a5b3482935518926434add67
websitetest <ricarde@mail.com>
2016-02-08 14:56:01 +0100

    Update config.php

--- config/config.php
+++ config/config.php
@@ -3,8 +3,8 @@
 define("BASE_PATH", $_SERVER['DOCUMENT_ROOT']);
 define("DIRSEP", DIRECTORY_SEPARATOR);
 
-define("DB_HOST", "localhost");
-define("DB_NAME", "c9");
-define("DB_USER", "janterhuijzen");
-define("DB_PWD", "c9");
+define("DB_HOST", "");
+define("DB_NAME", "");
+define("DB_USER", "");
+define("DB_PWD", "");
 ?>
